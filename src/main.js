2f5bbb975fc430b667e924a06cfdb30435ce6f06
Luminous <1094038421@qq.com>
2024-06-24 03:01:23 +0800

    实现登录、注册页面

--- src/main.js
+++ src/main.js
@@ -4,6 +4,7 @@ import App from './App.vue';
 import router from "./router/index.js";
 import '@/assets/css/common-layout.css';
 import '@/assets/css/problem-set.css';
+import '@/assets/css/login.css';
 import '@icon-park/vue-next/styles/index.css';
 
 createApp(App).use(router).mount('#app')
--- src/views/login/index.vue
+++ src/views/login/index.vue
@@ -1,9 +1,70 @@
 <script setup>
+import {ref, reactive} from "vue";
+import {useRouter} from "vue-router";
+
+const form = reactive({
+    username: '',
+    password: '',
+    verificationCode: '',
+})
+
+let activeName = ref('login');
+
+const router = useRouter();
+
+function onSubmitForLogin() {
+    router.push('/home');
+}
+
+function onSubmitForRegister() {
+    history.go(0);
+    router.push('/login');
+    activeName = 'login';
+}
 
 </script>
 
 <template>
-    <h1>Login</h1>
+    <div class="login-container">
+        <el-card>
+            <el-form
+                label-width="auto"
+                :model="form"
+            >
+                <div class="login-image">
+                    <img src="@/assets/image/background/login/login-image.png" alt="">
+                </div>
+
+                <div class="login-item">
+                    <el-tabs v-model="activeName" class="demo-tabs">
+                        <el-tab-pane label="Login" name="login">
+                            <span class="input-title">Username</span>
+                            <el-input v-model="form.username" placeholder=""/>
+
+                            <span class="input-title">Password</span>
+                            <el-input v-model="form.password" placeholder="" type="password"/>
+
+                            <el-button type="primary" @click="onSubmitForLogin">Login</el-button>
+                        </el-tab-pane>
+
+                        <el-tab-pane label="Register" name="register">
+                            <span class="input-title">Username</span>
+                            <el-input v-model="form.username" placeholder=""/>
+                            <span class="input-title">Password</span>
+                            <el-input v-model="form.password" placeholder="" type="password"/>
+                            <span class="input-title">Verification code</span>
+                            <div class="verification-code-item">
+                                <el-input v-model="form.verificationCode" placeholder=""/>
+                                <el-link type="primary">Verification code</el-link>
+                            </div>
+                            <el-button type="primary" @click="onSubmitForRegister">Register</el-button>
+                        </el-tab-pane>
+                    </el-tabs>
+                </div>
+
+            </el-form>
+        </el-card>
+    </div>
 </template>
 
 <style scoped>
